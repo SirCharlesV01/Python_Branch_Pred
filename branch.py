9da38c823d10ab14ad516bb8b94f365d0cd1b46a
Carlos Vargas <carlosvq1337@gmail.com>
2020-08-23 18:16:45 -0600

    get_stats function added, works great

--- branch.py
+++ branch.py
@@ -60,11 +60,19 @@ class bimodal_pred:
         print('Initializing bimodal predictor...')
         return
 
+    def get_jumps(self): #will return the predicted jumps **WIP
+        print('Predicting jumps...')
+        return
+
 class global_history_pred:
 
     def __init__(self, reg_size):
         print('Initializing global history predictor...')
         self.reg_size = reg_size
+        return
+
+    def get_jumps(self): #will return the predicted jumps **WIP
+        print('Predicting jumps...')
         return
 
 class private_history_pred:
@@ -74,27 +82,74 @@ class private_history_pred:
         self.reg_size = reg_size
         return
 
+    def get_jumps(self): #will return the predicted jumps **WIP
+        print('Predicting jumps...')
+        return
+
 class tournament_pred:
     
     def __init__(self):
         print('Initializing tournament predictor...')
         return
 
+    def get_jumps(self): #will return the predicted jumps **WIP
+        print('Predicting jumps...')
+        return
+
+
+
+
+
 #-----SIMULATOR CLASS-----#
 
 class simulator:
-    def __init__(self):
+
+    predicted_jump_list = []    #list of predicted jump values, filled by the predict_jump_values() function.
+
+    def __init__(self, cache):
         print('Initializing simulation...')
 
-        self.instruction_dir = []   #instruction directions list, to be read from trace file
-        self.jump_taken = []        #jump taken (T) or not taken (N) list, to be read from trace file
+        self.cache = cache
+
+        self.instruction_dir_list = []   #instruction directions list, to be read from trace file
+        self.actual_jump_list = []        #jump taken (T) or not taken (N) list, to be read from trace file
 
         print('Loading trace file...')
         for line in sys.stdin:      #Reading the decompressed trace file from pipe, saving columns into corresponding lists
             line = line.partition(' ')
-            self.instruction_dir.append(line[0])
-            self.jump_taken.append(line[-1])
+            self.instruction_dir_list.append(str.strip(line[0]))   #direction list appended
+            self.actual_jump_list.append(str.strip(line[-1]))      #actual jumps taken list appended
         return
+
+    def predict_jump_values(self):
+        self.predicted_jump_list = self.cache.predictor.get_jumps() #will return the predicted jump values (Taken or Not Taken) from the chosen predictor **WIP
+        return
+
+    def get_stats(self): #returns a dictionary with: correctly predicted taken branches, incorrectly predicted taken branches, correctly predicted not taken branches, incorrectly predicted not taken branches
+        stats = {
+            'CP_TB': 0,
+            'IP_TB': 0,
+            'CP_NB': 0,
+            'IP_NB': 0,
+            'CP_percentage': 0
+        }
+        print('Calculating stats...')
+        for (aj, pj) in zip(self.actual_jump_list, self.predicted_jump_list):
+            if aj == pj and pj == 'T':
+                stats['CP_TB'] += 1
+            elif aj != pj and pj == 'T':
+                stats['IP_TB'] += 1
+            elif aj == pj and pj == 'N':
+                stats['CP_NB'] += 1
+            elif aj == pj and pj == 'N':
+                stats['IP_NB'] += 1
+            else:
+                break
+
+        stats['CP_percentage'] = (len(self.actual_jump_list))*100/(stats['CP_TB'] + stats['CP_NB'])
+
+        return stats
+        
 
 
 
@@ -106,7 +161,13 @@ def main():
     cache_instance = cache(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
     #Initialize simulation
-    sim = simulator()
+    sim = simulator(cache_instance)
+
+    #Predict jumps
+    sim.predict_jump_values()
+
+    #Print results
+    sim.get_stats()
 
 if __name__ == "__main__":
         main()
